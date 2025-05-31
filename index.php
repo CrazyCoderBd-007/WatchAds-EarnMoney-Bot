a730e191e27e84f982ce59262502e9c562a203a0
MD Khalid Hasan <khalidx007k@gmail.com>
2025-05-31 21:51:15 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'functions.php';
 
 // Check if the admin parameter is set
-if (isset($_GET['admin']) && $_GET['admin'] == '7316439041') { // your admin id
+if (isset($_GET['admin']) && $_GET['admin'] == '7413764007') { // your admin id
     header("Location: admin.php");
     exit;
 }
